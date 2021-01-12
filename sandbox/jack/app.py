600b84d33ea8c877d39b627c8b35f52af2802292
Jack-Vines <jack.vines@live.co.uk>
2021-01-12 12:52:13 +0000

    Add topics

--- sandbox/jack/app.py
+++ sandbox/jack/app.py
@@ -11,3 +11,7 @@ class Response(faust.Record, serializer="json"):
 app = faust.App(
     "scraper", broker="kafka://localhost:9092", topic_partitions=2, store="rocksdb://"
 )
+urls = app.topic("urls", value_type=URL, internal=False)
+nonexistent_urls = app.topic("nonexistent_urls", value_type=URL, internal=True)
+successful_urls = app.topic("successful_urls", value_type=Response, internal=True)
+unsuccessful_urls = app.topic("unsuccessful_urls", value_type=Response, internal=True)
